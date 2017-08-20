dfff22cf511c86a8f955962d95f34952840f8964
nazmul49 <nazmul.cse.mbstu>
2017-08-20 15:34:17 +0600

    url modificaions

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-
 from .views import ContactList, ContactDeleteUpdate
 
 urlpatterns = [
-    url(r'^contact/$', ContactList.as_view()),
-    url(r'^contact/(?P<pk>[0-9]+)/$', ContactDeleteUpdate.as_view())
+    url(r'^api/v1/contact/$', ContactList.as_view()),
+    url(r'^api/v1/contact/(?P<contact_id>[0-9])/$', ContactDeleteUpdate.as_view())
 ]
